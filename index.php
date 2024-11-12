0a1752fc49da6e524ac5cbfabdedf07cd49a00d9
Rithika G <139311677+RithikaGunasekaran@users.noreply.github.com>
2024-11-12 10:33:37 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Online Shopping</title>
+    <link rel="stylesheet" href="styled.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+    <section class="header">
+        <div class="navi">
+        <nav>
+            
+            <div class="nav-links" id="navLinks">
+                <i class="fa-solid fa-cart-shopping"></i>
+                <i class="fa fa-times" onclick="hideMenu()"></i>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">About</a></li>
+                    <li><a href="">Featured</a></li>
+                    <li><a href="">Arrivals</a></li>
+                    <li><a href="">Reviews</a></li>
+                </ul>
+            </div>
+            <i class="fa fa-bars" onclick="showMenu()"></i>
+        </nav></div>
+<div class="textBox">
+    <h1>Upto 80% off</h1>
+    <h6>on Latest Arrivals,Publishers and Many More</h6>
+
+    <a href="./products.php" class="hero-btn">Shop Now</a>   
+
+</div>
+    </section>
+
+
+
+<section class="course">
+        
+        <div class="crow">
+    
+            <div class="courseCol">
+                <h3>Fast Delivery</h3>
+                <p>Our service guarantees prompt and reliable delivery for your orders, ensuring peace of mind every step of the way. </p>
+            </div>
+            <div class="courseCol">
+                <h3>24x7 Services</h3>
+                <p>You can shop for books anytime, day or night, ensuring you never have to wait to satisfy your literary cravings.</p>
+            </div>
+            <div class="courseCol">
+                <h3>Convenience</h3>
+                <p>No more rushing to the bookstore before closing time or waiting for the next day's delivery. </p>
+            </div>
+        </div>
+</section>
+
+
+<div class="aboutus">
+    <div class="row">
+        <div class="imgCol">
+            <img src="./Books_1.jpg">
+        </div>
+        <div class="textCol">
+            <h2>About Us</h2>
+            <p>We envision a world where everyone has access to the books they love, regardless of time or location. Through our 24/7 online bookstore, we strive to make the magic of books accessible to readers around the globe, fostering a community of lifelong learners and passionate bibliophiles.
+            We believe that books have the unique ability to educate, entertain, and enlighten, and we're dedicated to connecting readers with the stories and ideas that matter most to them.
+            </p>
+        </div>
+
+    </div>
+</div>
+
+
+
+
+
+
+<!--<section>
+    <div class="rows">
+        <h2>Featured </h2>
+        <div class="rowPosters">
+            <img src="/images/book1.jpg" class="rowPoster rowPosterlarge">
+            <img src="/images/book2.jpg" class="rowPoster rowPosterlarge">
+            <img src="/images/book3.jpg" class="rowPoster rowPosterlarge">
+            <img src="/images/book4.jpg" class="rowPoster rowPosterlarge">
+            <img src="/images/book5.jpg" class="rowPoster rowPosterlarge">
+            <img src="/images/book6.jpg" class="rowPoster rowPosterlarge">
+            <img src="/images/book1.jpg" class="rowPoster rowPosterlarge">
+            <img src="/images/book2.jpg" class="rowPoster rowPosterlarge">
+            <img src="/images/book3.jpg" class="rowPoster rowPosterlarge">
+            <img src="/images/book4.jpg" class="rowPoster rowPosterlarge">
+            <img src="/images/book5.jpg" class="rowPoster rowPosterlarge">
+            <img src="/images/book6.jpg" class="rowPoster rowPosterlarge">
+            <img src="/images/book1.jpg" class="rowPoster rowPosterlarge">
+          
+        </div>
+    </div>
+</section>
+-->
+
+
+
+<div class="aboutus">
+    <div class="row">
+        <div class="textCol">
+            <h2>Digital Book</h2>
+            <p>Enjoy the convenience of digital reading with our wide range of ebooks, perfect for reading on your favorite device. </p>
+        </div>
+        <div class="imgCol">
+            <img src="./ebook1.jpg">
+        </div>
+        <a href="" class="hero-btn">Learn More</a>
+
+    </div>
+</div>
+
+<div class="footer">
+    <h2>Questions? call 000-000-004-000</h2>
+    <div class="row">
+        <div class="col">
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Featured</a>
+            <a href="#">Arrivals</a>
+
+        </div>
+        <div class="col">
+            <a href="#">Help Center</a>
+            <a href="#">+94 0842 0462</a>
+            <a href="#">bookstore@gmail.com</a>
+            
+
+        </div>
+        <div class="col">
+            <a href="#">Accounts</a>
+            <a href="#">Sign In</a>
+            <a href="#">Payments</a>
+            <a href="#">Only on Bookstore</a>
+
+        </div>
+        
+    </div><p class="copyright">© BookStore India</p>
+</div>
+</body>
+</html>
